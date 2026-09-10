c5daa17972507f67629ab71ab41b83a932ddd759
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-10 14:29:45 +0900

    mongoDBのサーバー起動ロジック追加

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/smokingArea')
+    .then(() => {
+        console.log("コネクションOK");
+    })
+    .catch((err) => {
+        console.error("コネクションエラー");
+        console.error(err);
+    });
 
 app.get("/", (req, res) => {
     res.send("こんにちは");
