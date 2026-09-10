3c1f14a99a98c7cadff8eaaa1af4398341e93f75
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-10 14:28:41 +0900

    getリクエストの確認

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("こんにちは");
+});
+
 app.listen(3001, () => {
     console.log("ポート3001でサーバー起動");
 });
